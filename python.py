aeee5e384bc4f81d9aa0a1c9212d86dcdaae9e97
wintoo <wintoo143@gmail.com>
2017-06-28 23:29:08 -0500

    Adding python.py script file in repository

---
+++ python.py
@@ -0,0 +1,10 @@
+foods = ['Chicken', 'Fish', 'Prawns', 'Pork', 'Beef']
+vegetables = ['Tomato', 'Onion', 'Carrot', 'Bringal']
+
+# using for loop we are name items as foods variable.
+# using print(len()), we are printing the length of the characters in items.
+
+for items in foods:
+	print (items)
+	print (len(items))
+
