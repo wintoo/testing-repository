247effa9efeec4b34e4bf394630cd858656934be
wintoo <wintoo143@gmail.com>
2017-06-28 23:45:16 -0500

    updated python.py file

--- python.py
+++ python.py
@@ -1,5 +1,7 @@
 foods = ['Chicken', 'Fish', 'Prawns', 'Pork', 'Beef']
 vegetables = ['Tomato', 'Onion', 'Carrot', 'Bringal']
+salads = ['Mango salad', 'Apple salad']
+
 
 # using for loop we are name items as foods variable.
 # using print(len()), we are printing the length of the characters in items.
